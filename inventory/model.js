8f14f8f3db1f5019cbdcc5c634d6da0fd8c7201c
Andrew Park <13parksangjoon@gmail.com>
2019-01-11 14:02:05 -0800

    login/register routers

---
+++ inventory/model.js
@@ -0,0 +1,29 @@
+'use strict';
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+const inventorySchema = mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'users' },
+  bookList: [{
+      title: String,
+      author: String,
+      stock: Number,
+      price: Number
+  }]
+});
+
+inventorySchema.pre('find', function(next) {
+    this.populate('user');
+    next();
+ });
+ 
+ inventorySchema.pre('findOne', function(next) {
+    this.populate('user');
+    next();
+ });
+
+
+const Inventory = mongoose.model('inventories', inventorySchema);
+
+module.exports = {Inventory};
